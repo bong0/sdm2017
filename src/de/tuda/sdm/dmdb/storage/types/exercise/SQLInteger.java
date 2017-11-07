5b9f9cd7c77fb92d28f5954737910f11a9081c86
Julian Kessel <mail@juliankessel.de>
2017-11-07 13:24:41 +0100

    added simplifications to storing; using polymorphy

--- src/de/tuda/sdm/dmdb/storage/types/exercise/SQLInteger.java
+++ src/de/tuda/sdm/dmdb/storage/types/exercise/SQLInteger.java
@@ -17,7 +17,6 @@ public class SQLInteger extends SQLIntegerBase {
 	 * Constructor with default value
 	 */
 	public SQLInteger(){ super();
-		this.setMaxLength(8); // we need more space due to attribute metadata
 	}
 
 	/**
@@ -26,15 +25,13 @@ public class SQLInteger extends SQLIntegerBase {
 	 */
 	public SQLInteger(int value){
 		super(value);
-		this.setMaxLength(8); // we need more space due to attribute metadata
 	}
 
-	private static short meta_offset = 2*Short.BYTES;// skip bytes from attribute metadata
 
 
 	@Override
 	public byte[] serialize() {
-		byte[] attrMetaBytes = RowPage.genAttributeMetaBytes(RowPage.DATATYPE_INT, (short)0);
+		//byte[] attrMetaBytes = RowPage.genAttributeMetaBytes(RowPage.DATATYPE_INT, (short)0);
 		byte[] valueBytes = new byte[] {
 				// use >>> operator for right shifting so zeroes are carried in from left
 				(byte) (this.value >>> 24), // lowest mem addr => MSB
@@ -43,34 +40,25 @@ public class SQLInteger extends SQLIntegerBase {
 				(byte) ((this.value << 24) >>> 24) // highest mem addr => LSB
 		};
 		// concatenate the two arrays
-		byte[] out = new byte[attrMetaBytes.length+valueBytes.length];
-		System.arraycopy(attrMetaBytes, 0, out, 0, attrMetaBytes.length);
-		System.arraycopy(valueBytes,0, out, attrMetaBytes.length, valueBytes.length);
+		byte[] out = new byte[valueBytes.length];
+		System.arraycopy(valueBytes,0, out, 0, valueBytes.length);
 
 		return out;
 	}
 
 	@Override
 	public void deserialize(byte[] data) {
-
-		byte[] metabytes = new byte[2*Short.BYTES];
-		System.arraycopy(
-				data, 0,
-				metabytes, 0,
-				2*Short.BYTES
-		);
-		RowPage.AttributeMetadata meta = RowPage.extractAttributeMetadata(metabytes);
-
-		if(meta.datatype != RowPage.DATATYPE_INT){
-			throw new RuntimeException("Expected datatype in attribute metadata to be INT");
-		} else if (meta.maxlen != 0){
-			throw new RuntimeException("Expected unused maxlen attribute in metadata to be 0");
+		if(data == null){
+			throw new IllegalArgumentException("Cannot deserialize null value");
+		}
+		if(data.length != this.getFixedLength()){
+			throw new IllegalArgumentException("You passed an invalid number of bytes, expected "+this.getFixedLength());
 		}
 		// interpret bytes as unsigned through masking with AND 0xFF to get lower part
-		this.value =(data[meta_offset+0] & 0xFF) << 24 |
-			(data[meta_offset+1]& 0xFF) << 16 |
-			(data[meta_offset+2]& 0xFF) << 8 |
-			(data[meta_offset+3]& 0xFF);
+		this.value =(data[0] & 0xFF) << 24 |
+			(data[1]& 0xFF) << 16 |
+			(data[2]& 0xFF) << 8 |
+			(data[3]& 0xFF);
 	}
 
 	@Override
@@ -78,12 +66,4 @@ public class SQLInteger extends SQLIntegerBase {
 		return new SQLInteger(this.value);
 	}
 
-	@Override
-	public int getFixedLength() {
-		return 8; //fixed length -- different from base class since we work with the attribute metadata
-	}
-
 }
-
-
-/* TODO: write test that checks whether serialized length is fixed length */
