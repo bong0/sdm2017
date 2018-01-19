86d0113cf4514ee4d8599499ce4e976f297eb344
Julian Kessel <mail@juliankessel.de>
2018-01-19 13:20:39 +0100

    partial implementation of the exc's mandatory task; test still failing

--- ex09/src/de/tuda/sdm/dmdb/sql/operator/exercise/Receive.java
+++ ex09/src/de/tuda/sdm/dmdb/sql/operator/exercise/Receive.java
@@ -1,6 +1,10 @@
 package de.tuda.sdm.dmdb.sql.operator.exercise;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
 
 import de.tuda.sdm.dmdb.net.TCPServer;
 import de.tuda.sdm.dmdb.sql.operator.Operator;
@@ -27,32 +31,68 @@ public class Receive extends ReceiveBase {
 
 	@Override
 	public void open() {
-		// TODO: implement this method
+
+
+
+		try {
+			this.receiveServer = new TCPServer(listenerPort, localCache, finishedPeers);
+			Thread serveThread = new Thread(receiveServer);
+			serveThread.start();
+		} catch (IOException e) {
+			System.err.println("FATAL: Could not start receive server on node "+this.nodeId);
+		}
+
 		// HINT: local cache must be passed to TCPServer
 		//       and will be accessed by multiple Handler-Threads - take multi-threading into account where applicable!
-				
+
 		// init local cache
+		this.localCache = new LinkedBlockingDeque<>();
 
 		// Attention: call open on child after starting receive server, so that sendOperator can connect
+		System.out.println("Starting  server recv for id "+this.nodeId);
+		this.child.open();
 
 	}
 
 	@Override
 	public AbstractRecord next() {
-		// TODO: implement this method
 		// HINT: local cache must be passed to TCPServer
 		//       and will be accessed by multiple Handler-Threads - take multi-threading into account where applicable!
 		// process local and received records...
-		
-			// check if we finished processing of all records - hint: you can use this.finishedPeers
 
-		return null;
+		AbstractRecord nextLocalItem = this.child.next();
+		if(nextLocalItem != null){
+			return nextLocalItem;
+		}
+
+		// check if we finished processing of all records - hint: you can use this.finishedPeers
+		if(this.finishedPeers.get() < this.numPeers){
+
+
+			AbstractRecord nextRemoteItem = null;
+			synchronized (localCache) { // make this operation atomic
+				try {
+					System.out.println("wait blocking");
+					nextRemoteItem = ((BlockingQueue<AbstractRecord>)this.localCache).take();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			if(nextRemoteItem != null){
+				return nextRemoteItem;
+			} else {
+				System.err.println("ERROR: Queue should be filled but poll returned null; this shouldn't happen");
+			}
+		}
+
+		return null; // all peers and local finished
 	}
 
 	@Override
 	public void close() {
-		// TODO: implement this method
 		// reverse what was done in open()
+		this.child.close();
+		this.receiveServer.stopServer();
 	}
 
 }
--- ex09/src/de/tuda/sdm/dmdb/sql/operator/exercise/Send.java
+++ ex09/src/de/tuda/sdm/dmdb/sql/operator/exercise/Send.java
@@ -23,40 +23,60 @@ public class Send extends SendBase {
 	 * @param nodeMap - Map containing connection information (as "IP:port" or "domain-name:port") to establish connection to other peers
 	 * @param partitionColumn - Number of column that should be used to repartition the data
 	 */
+
 	public Send(Operator child, int nodeId, Map<Integer,String> nodeMap, int partitionColumn) {
 		super(child, nodeId, nodeMap, partitionColumn);
 	}
 
 	@Override
 	public void open() {
-		// TODO: implement this method
 		// init child
+		child.open(); // init underlying OP (tablescan/selection...)
 
 		// create a client socket for all peer nodes using information in nodeMap
-		// store client socket in map for later use
+		for(Integer nodeId : nodeMap.keySet()){
+			String[] connInfoSplit = nodeMap.get(nodeId).split(":");
+			String host = connInfoSplit[0];
+			Integer port = Integer.parseInt(connInfoSplit[1]);
+			try {
+				// store client socket in map for later use
+				this.connectionMap.put(nodeId, new TCPClient(host,port));
+			} catch (IOException e) {
+				System.err.println("FATAL: Could not open connection from peer "+this.nodeId+" to peer "+nodeId);
+			}
+		}
 
 	}
 
 	@Override
 	public AbstractRecord next() {
-		// TODO: implement this method
 		// retrieve next from child and determine whether to keep record local or send to peer
 
-				// store locally
+		AbstractRecord nextRec = this.child.next();
+		if(nextRec == null) {
+			// reached end, close connections to peers
+			closeConnectionsToPeers();
+			return null;
+		} // serving records finished
 
-				// send to a peer
+		Integer destinedNodeId = this.getNodeIdForRecord(nextRec, this.partitionColumn);
 
+		if(destinedNodeId != this.nodeId){
+			// send to a peer
+			this.connectionMap.get(destinedNodeId).sendRecord(nextRec);
+		} else {
+			return nextRec;
+		}
 
-		// reached end, close connections to peers
-
-
-		return null;
+		return null; // record was sent to remote, semantic is changed here; it doesn't indicate that send finished
 	}
 
 	@Override
 	public void close() {
 		// TODO: implement this method
 		// reverse what was done in open() - hint there is a helper method that you can use
+		closeConnectionsToPeers();
+		child.close();
 	}
 
 }
